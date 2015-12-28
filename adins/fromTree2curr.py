9651de8f66a4779faf90dcafbc5e7d36953255e5
Ukrpost <ukrpost@gmail.com>
2015-12-28 18:13:16 +0000

    testing

--- adins/fromTree2curr.py
+++ adins/fromTree2curr.py
@@ -44,7 +44,6 @@ in_one(sys.argv[1])
 #     # print path to all subdirectories first.
 #     # for subdirname in dirnames:
 #     #     print(os.path.join(dirname, subdirname))
-
 #     # print path to all filenames.
 #     for filename in filenames:
 #         print(os.path.join(dirname, filename))
